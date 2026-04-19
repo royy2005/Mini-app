802eebec8038ee2b691cca5b8beed04bbed49e1f
royy2005 <ronniercabrera@gmail.com>
2026-04-19 17:52:42 -0600

    add App tsx.

--- src/server/lib/src/server/src/server/lib/src/server/routes/src/client/src/client/main.tsx
+++ src/server/lib/src/server/src/server/lib/src/server/routes/src/client/src/client/main.tsx
@@ -1,15 +1,5 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App";
-import "./index.css";
+import Home from "./Home";
 
-const queryClient = new QueryClient();
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-      <QueryClientProvider client={queryClient}>
-            <App />
-                </QueryClientProvider>
-                  </StrictMode>
-                  );
+export default function App() {
+  return <Home />;
+  }
